3dae8c51241e93719025bd9e83db59d6bdd47792
Cheburekk <bulbulbul981@gmail.com>
2021-02-03 19:55:09 +0200

    Added left_to_right_check and check_horizontal_visibility functions

--- lab1/skyscrapers.py
+++ lab1/skyscrapers.py
@@ -24,7 +24,60 @@ def check_not_finished_board(board: list):
     >>> check_not_finished_board(['***21**', '412453*', '423145*', '*5?3215', '*35214*', '*41532*', '*2*1***'])
     False
     """
-    for line in board:
-        if '?' in line:
+    for row in board:
+        if '?' in row:
             return False
     return True
+
+
+def left_to_right_check(input_line: str, pivot: int):
+    """
+    Check row-wise visibility from left to right.
+    Return True if number of building from the left-most hint is visible looking to the right,
+    False otherwise.
+
+    input_line - representing board row.
+    pivot - number on the left-most hint of the input_line.
+
+    >>> left_to_right_check("412453*", 4)
+    True
+    >>> left_to_right_check("452453*", 5)
+    False
+    """
+    current_highest = 0
+    visible_buildings = 0
+    for height in input_line[1:-1]:
+        if int(height) > current_highest:
+            current_highest = int(height)
+            visible_buildings += 1
+    if visible_buildings == int(pivot):
+        return True
+    return False
+
+
+def check_horizontal_visibility(board: list):
+    """
+    Check row-wise visibility (left-right and vice versa)
+
+    Return True if all horizontal hints are satisfiable,
+     i.e., for line 412453* , hint is 4, and 1245 are the four buildings
+      that could be observed from the hint looking to the right.
+
+    >>> check_horizontal_visibility(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    True
+    >>> check_horizontal_visibility(['***21**', '452453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    False
+    >>> check_horizontal_visibility(['***21**', '452413*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    False
+    """
+    for row in board[1:-1]:
+        if row[0] != '*' and not left_to_right_check(row, row[0]):
+            print(left_to_right_check(row, row[0]), row)
+            return False
+        row = row[::-1]
+        if row[0] != '*' and not left_to_right_check(row, row[0]):
+            print(left_to_right_check(row, row[0]), row)
+            return False
+    return True
+
+

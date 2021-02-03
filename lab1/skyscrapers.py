e727140f4c1e634ce456a1382a5591177deca686
Cheburekk <bulbulbul981@gmail.com>
2021-02-03 17:02:59 +0200

    Added read_input and check_not_finished_board functions

---
+++ lab1/skyscrapers.py
@@ -0,0 +1,30 @@
+def read_input(path: str):
+    """
+    Read game board file from path.
+    Return list of str.
+
+    >>> read_input("check.txt")
+    ['***21**', '452453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']
+    """
+    with open(path) as board:
+        lines = board.read().split('\n')[:-1]
+    return lines
+
+
+def check_not_finished_board(board: list):
+    """
+    Check if skyscraper board is not finished, i.e., '?' present on the game board.
+
+    Return True if finished, False otherwise.
+
+    >>> check_not_finished_board(['***21**', '4?????*', '4?????*', '*?????5', '*?????*', '*?????*', '*2*1***'])
+    False
+    >>> check_not_finished_board(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    True
+    >>> check_not_finished_board(['***21**', '412453*', '423145*', '*5?3215', '*35214*', '*41532*', '*2*1***'])
+    False
+    """
+    for line in board:
+        if '?' in line:
+            return False
+    return True
